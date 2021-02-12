7e23e28f323354adc575434e028718532b690cc0
bhawk3 <brendanhawk03@aol.com>
2021-02-11 19:47:41 -0500

    Add project files.

---
+++ TicTacToeProject1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace TicTacToeProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.oRadioButton = new System.Windows.Forms.RadioButton();
+            this.xRadioButton = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hasSomeoneWonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clickForWinnerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 68);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(233, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(345, 262);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(208, 169);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(95, 68);
+            this.button9.TabIndex = 7;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(107, 169);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(95, 68);
+            this.button8.TabIndex = 6;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 169);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(95, 68);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(208, 95);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 68);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(107, 95);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 68);
+            this.button5.TabIndex = 3;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 68);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(208, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 68);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 68);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.oRadioButton);
+            this.groupBox2.Controls.Add(this.xRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(649, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(123, 66);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Whose turn?";
+            // 
+            // oRadioButton
+            // 
+            this.oRadioButton.AutoSize = true;
+            this.oRadioButton.Location = new System.Drawing.Point(47, 30);
+            this.oRadioButton.Name = "oRadioButton";
+            this.oRadioButton.Size = new System.Drawing.Size(40, 21);
+            this.oRadioButton.TabIndex = 1;
+            this.oRadioButton.TabStop = true;
+            this.oRadioButton.Text = "O";
+            this.oRadioButton.UseVisualStyleBackColor = true;
+            this.oRadioButton.CheckedChanged += new System.EventHandler(this.oRadioButton_CheckedChanged);
+            // 
+            // xRadioButton
+            // 
+            this.xRadioButton.AutoSize = true;
+            this.xRadioButton.Location = new System.Drawing.Point(6, 30);
+            this.xRadioButton.Name = "xRadioButton";
+            this.xRadioButton.Size = new System.Drawing.Size(35, 21);
+            this.xRadioButton.TabIndex = 0;
+            this.xRadioButton.TabStop = true;
+            this.xRadioButton.Text = "x";
+            this.xRadioButton.UseVisualStyleBackColor = true;
+            this.xRadioButton.CheckedChanged += new System.EventHandler(this.xRadioButton_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hasSomeoneWonToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hasSomeoneWonToolStripMenuItem
+            // 
+            this.hasSomeoneWonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clickForWinnerToolStripMenuItem});
+            this.hasSomeoneWonToolStripMenuItem.Name = "hasSomeoneWonToolStripMenuItem";
+            this.hasSomeoneWonToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.hasSomeoneWonToolStripMenuItem.Text = "Has someone won?";
+            // 
+            // clickForWinnerToolStripMenuItem
+            // 
+            this.clickForWinnerToolStripMenuItem.Name = "clickForWinnerToolStripMenuItem";
+            this.clickForWinnerToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.clickForWinnerToolStripMenuItem.Text = "Click for winner";
+            this.clickForWinnerToolStripMenuItem.Click += new System.EventHandler(this.clickForWinnerToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton xRadioButton;
+        private System.Windows.Forms.RadioButton oRadioButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hasSomeoneWonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clickForWinnerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+    }
+}
+
---
+++ TicTacToeProject1/Form1.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+/*project:tic tac toe
+ * programmer:Brendan Hawk
+ * date:11/23/2020
+ * description: tic tac toe game with radio button stating whose turn and menu to click for winner
+ */
+
+namespace TicTacToeProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int player1Click = 0, turn1 = 0;
+        int winner = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+
+            if (turn1 == 0)
+            {
+                button1.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+                {
+                button1.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button2.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button2.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button3.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button3.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button4.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button4.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button5.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button5.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button6.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button6.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button7.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button7.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button8.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button8.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void xRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //display if it is player X turn
+
+
+        }
+
+        private void oRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            //display if it is play O turn
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //Display X or O when depending on which user clicks
+
+            if (turn1 == 0)
+            {
+                button9.Text = "X";
+                turn1++;
+                xRadioButton.Checked = true;
+                oRadioButton.Checked = false;
+            }
+            else if (turn1 == 1)
+            {
+                button9.Text = "O";
+                turn1--;
+                xRadioButton.Checked = false;
+                oRadioButton.Checked = true;
+            }
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //About the programmer
+
+            MessageBox.Show("Tic Tac Toe Project By Brendan Hawk");
+        }
+
+        private void clickForWinnerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //Check for a winner
+            //check if X is a winner
+            while (true)
+            {
+                if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+                {
+                    MessageBox.Show("X is the winner");
+                    winner++;
+                }
+                break;
+            }
+
+            //set the O options for winning
+
+            while (true)
+            {
+                if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+
+            while (true)
+            {
+                if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+                {
+                    MessageBox.Show("O is the winner");
+                    winner--;
+                }
+                break;
+            }
+
+
+            if (winner == 0)
+            {
+                MessageBox.Show("Cat game");
+            }
+
+        }
+
+        
+    }
+}
